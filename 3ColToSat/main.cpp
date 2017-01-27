382d56419675b1627a6278573e622f81edb379bc
Marino <mabrgr@gmail.com>
2017-01-27 14:54:16 +0100

    added map with edges in the graph

--- 3ColToSat/main.cpp
+++ 3ColToSat/main.cpp
@@ -11,8 +11,17 @@ int main() {
 	cin >> nVer;
 
 	set<string> vertices;
+	map<string, set<string>> edges;
 
 	for (int i = 0; i < nVer; i++) vertices.insert(string(1, (char(i + 'A')))); //Inserting vertices into the set, vertcies labeled alphabetically from A
 
-
+	//Adding edges to the map edges. (v1 and v2 are vertcies connected by an edge)
+	while (true) {
+		string v1, v2;
+		cout << "Enter connecting edges, space between the edges. Enter '0' number to finish: ";
+		cin >> v1, v2;
+		if (v1 == "0") break;
+		edges[v1].insert(v2); //Add edge from e1 to e2
+		edges[v2].insert(v1); //Add edge from e2 to e1
+	}
 }
