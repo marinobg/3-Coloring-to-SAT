2d0b1967aa57b40753aed52acd93a81c4f7c8b11
Marino <mabrgr@gmail.com>
2017-01-27 13:54:58 +0100

    added files

---
+++ 3ColToSat/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<set>
+#include<string>
+#include<map>
+
+using namespace std;
+
+int main() {
+	unsigned int nVer;
+	cout << "Enter number of vertices in the graph: ";
+	cin >> nVer;
+
+	set<string> vertices;
+
+	for (int i = 0; i < nVer; i++) vertices.insert(string(1, (char(i + 'A')))); //Inserting vertices into the set, vertcies labeled alphabetically from A
+
+
+	for (auto it = vertices.begin(); it != vertices.end(); it++) {
+		cout << *it << endl;
+	}
+	system("PAUSE");
+}
